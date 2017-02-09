9df33e5bd0ee26056d6dbbe924cedd7326cf4c22
Airwoman <airwomanzhy@hotmail.com>
2017-02-09 22:16:09 +1100

    change database setting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315235724) do
+ActiveRecord::Schema.define(version: 20170201114924) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
